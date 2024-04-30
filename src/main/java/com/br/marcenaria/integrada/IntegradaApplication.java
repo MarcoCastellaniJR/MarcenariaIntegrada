5043dffb6184da3e908f3d09a147166229ceddd7
Marco Castellani <marcocastellanictt@gmail.com>
2024-04-30 16:17:08 -0300

    Projeto Base

---
+++ src/main/java/com/br/marcenaria/integrada/IntegradaApplication.java
@@ -0,0 +1,13 @@
+package com.br.marcenaria.integrada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegradaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegradaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/marcenaria/integrada/IntegradaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.marcenaria.integrada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegradaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
